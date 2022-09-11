f8f09e07b33b113dd59bba4dc34a9bbb1e228eac
lionbalogun <lionbalogun@yahoo.com>
2022-09-11 20:59:33 +0100

    Aplhabet Upper and Lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - putchar print Uppercase and lowercase alphabets
+ *
+ * Description: using the main function
+ * this program prints Uppercase and Lowercase Alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+char ch;
+    
+for (ch= 'A'; ch<= 'Z'; ch++)
+{
+putchar(ch);
+}
+for (ch= 'a'; ch<= 'z'; ch++)
+{
+putchar(ch);
+}   
+
+putchar('\n');
+
+return (0);
+}
