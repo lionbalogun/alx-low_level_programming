dd3aef7f457e10b56e3eb1343eb6d863ecf51664
lionbalogun <lionbalogun@yahoo.com>
2022-09-11 17:25:56 +0100

    Print Lower case Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main -Entry point
+ *
+ * my code
+ * Return: sucess always
+ */
+
+int main(void)
+{
+for (int x = 'A'; x <= 'Z'; x++)
+    {
+    int lower_x = tolower(x);
+    putchar(lower_x);
+    }
+    return 0;
+}
+
