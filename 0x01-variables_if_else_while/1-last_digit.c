3b5d144590ab285b0fc9057fd21c9d887c699f38
lionbalogun <lionbalogun@yahoo.com>
2022-09-11 05:03:27 +0100

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,8 +25,8 @@ int main(void)
             printf("%d and is 0 ", n);
         }
     if (n % 10 < 6)
-        {
+            {
             printf("%d is less than 6 and not 0 ", n);
-        }
+            }
     return (0);
 }
