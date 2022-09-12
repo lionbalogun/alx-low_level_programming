3efc5860914854b849b347a2fd86af699a42e3d1
lionbalogun <lionbalogun@yahoo.com>
2022-09-12 01:31:54 +0100

    Print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - putchar print Uppercase and lowercase alphabets
+ *
+ * Description: using the main function
+ * this program prints Uppercase and Lowercase Alphabets
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i=0; i<10; i++)
+{
+    printf("%d",i);
+}
+putchar('\n');
+return (0);
+}
