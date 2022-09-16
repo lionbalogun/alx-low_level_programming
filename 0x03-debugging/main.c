4aac084229e563498e839041e6cbb7e1b8e35361
lionbalogun <lionbalogun@gmail.com>
2022-09-15 17:04:03 -0700

    Debugging Project

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - test function that prints if integer is positive or negative
+* return 0
+*/
+
+int main(void)
+{
+int 1;
+
+i = 0;
+positive_or_negative9I0;
+
+return (0);
+}
