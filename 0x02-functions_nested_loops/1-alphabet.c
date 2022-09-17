13b2c81e781258cae90c6ee46d2c88c62619035c
lionbalogun <lionbalogun@gmail.com>
2022-09-16 18:43:46 -0700

    Loop project fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
- * print_alphabet - Prints the alphabet in lowercase
- *
- * Return : Always 0
+ * print_alphabet - function to print abc
+ * Return: 0
  */
 
 void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c < 'z'; c++)
-{
-_putchar(c);
-}
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 _putchar('\n');
 }
