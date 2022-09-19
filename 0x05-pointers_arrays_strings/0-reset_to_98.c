a851c7b3383b69c6c410883774ad8b98b1968a73
lionbalogun <lionbalogun@gmail.com>
2022-09-19 12:15:39 -0700

    Pointer task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+	int *p;
+
+	n = 402;
+	p = &n;
+
+	*p = 98;
+
+	return (0);
+}
+
