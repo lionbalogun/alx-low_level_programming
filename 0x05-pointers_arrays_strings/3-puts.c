5ac5f18684ab94a08d8d4f7b3522922b948c1eca
lionbalogun <lionbalogun@gmail.com>
2022-09-19 20:47:15 -0700

    Task 2 and 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to standard output
+ * @str: the string to be printed
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
